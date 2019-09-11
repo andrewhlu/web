ae6cf969ab9a43419d1d63f6b06d2870b22ef329
Andrew Lu <alu@ucsb.edu>
2019-09-11 11:13:54 -0400

    test png

--- js/ramen.js
+++ js/ramen.js
@@ -1,75 +1,75 @@
 var rankings = [
 	{
 		title: "Sonos Interns Take On Ramen!",
-		url: "/images/ramen/bigbowls.jpg"
+		url: "/images/ramen/png/bigbowls.png"
 	},
 	{
 		title: "1. Santouka",
-		url: "/images/ramen/01santouka.jpg"
+		url: "/images/ramen/png/01santouka.png"
 	},
 	{
 		title: "2. Isshindo Ramen",
-		url: "/images/ramen/02isshindo.jpg"
+		url: "/images/ramen/png/02isshindo.png"
 	},
 	{
 		title: "3 (tie). Oisa Ramen",
-		url: "/images/ramen/03oisa.jpg"
+		url: "/images/ramen/png/03oisa.png"
 	},
 	{
 		title: "3 (tie). Tsurumen Davis",
-		url: "/images/ramen/03tsurumen.jpg"
+		url: "/images/ramen/png/03tsurumen.png"
 	},
 	{
 		title: "4. Ganko Ittetsu Ramen",
-		url: "/images/ramen/04gankoittetsu.jpg"
+		url: "/images/ramen/png/04gankoittetsu.png"
 	},
 	{
 		title: "5. Shabu & Mein",
-		url: "/images/ramen/05shabumein.jpg"
+		url: "/images/ramen/png/05shabumein.png"
 	},
 	{
 		title: "6. Shin Hakata Ramen",
-		url: "/images/ramen/06shinhakata.jpg"
+		url: "/images/ramen/png/06shinhakata.png"
 	},
 	{
 		title: "7. Snappy Kitchen",
-		url: "/images/ramen/07snappy.jpg"
+		url: "/images/ramen/png/07snappy.png"
 	},
 	{
 		title: "8. Sapporo Ramen",
-		url: "/images/ramen/08sapporo.jpg"
+		url: "/images/ramen/png/08sapporo.png"
 	},
 	{
 		title: "9. One Ramen And Sushi",
-		url: "/images/ramen/09oneramen.jpg"
+		url: "/images/ramen/png/09oneramen.png"
 	},
 	{
 		title: "10. Sakura Sunnaku",
-		url: "/images/ramen/10sakurasunnaku.jpg"
+		url: "/images/ramen/png/10sakurasunnaku.png"
 	},
 	{
 		title: "11. Yume Wo Katare",
-		url: "/images/ramen/11yumewokatare.jpg"
+		url: "/images/ramen/png/11yumewokatare.png"
 	},
 	{
 		title: "12. Totto Ramen",
-		url: "/images/ramen/12totto.jpg"
+		url: "/images/ramen/png/12totto.png"
 	},
 	{
 		title: "13. Ruckus",
-		url: "/images/ramen/13ruckus.jpg"
+		url: "/images/ramen/png/13ruckus.png"
 	},
 	{
 		title: "14. Wen's Yunnan Noodle and Ramen",
-		url: "/images/ramen/14wensyunnan.jpg"
+		url: "/images/ramen/png/14wensyunnan.png"
 	},
 	{
 		title: "15. Wagamama",
-		url: "/images/ramen/15wagamama.jpg"
+		url: "/images/ramen/png/15wagamama.png"
 	},
 	{
 		title: "16. Amateras",
-		url: "/images/ramen/16amateras.jpg"
+		url: "/images/ramen/png/16amateras.png"
 	}
 ];
 
